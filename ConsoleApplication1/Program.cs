758075e7d6e982f11494b3250912b2c4b00f1f82
the-war-horse <jameslrsmith@gmail.com>
2017-08-08 12:03:39 +0100

    balls

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.IO;
-using System.Text;
-using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
-using System.ComponentModel;
 using Microsoft.VisualBasic;
+using WindowsInput;
 
 namespace WinMan_Loader
 {
@@ -17,58 +14,62 @@ namespace WinMan_Loader
         // import the function in your class
         //[DllImport("User32.dll")]
         //static extern int SetForegroundWindow(IntPtr point);
-        [DllImport("User32.dll", SetLastError = true)]
-        private static extern int SetForegroundWindow(IntPtr hWnd);
+        public const int SW_RESTORE = 9;
 
         [DllImport("user32.dll")]
-        private static extern Boolean ShowWindow(IntPtr hWnd);
+        public static extern bool IsIconic(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, IntPtr ProcessId);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr AttachThreadInput(IntPtr idAttach,
+                             IntPtr idAttachTo, bool fAttach);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetFocus();
 
         //...
-        [DllImport("user32.dll")]
-        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags,
-        UIntPtr dwExtraInfo);
 
-
-        private static void Main()
+        private static void Main(string[] args)
         {
             String path = @"c:\temp\Password.txt";
             String oPass = File.ReadAllText(path);
 
-            Process p = Process.Start(@"\\VS02\WinManV7\Live\WinMan.exe");
+            Process p = Process.Start(@"\\VS02\WinManV7\Live\WinMan.exe");           
             p.WaitForInputIdle();
             IntPtr h = p.MainWindowHandle;
             while (p.MainWindowHandle == IntPtr.Zero)
             {
                 System.Threading.Thread.Sleep(100);
             }
-
+            
             if (Control.IsKeyLocked(Keys.CapsLock)) // Checks Capslock is on
-            {
-                const int KEYEVENTF_EXTENDEDKEY = 0x1;
-                const int KEYEVENTF_KEYUP = 0x2;
-                keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
-                keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,(UIntPtr)0);
+            {                
+                InputSimulator.SimulateKeyDown(VirtualKeyCode.SHIFT);                
                 ActivateApp("winman.exe");
-                ActivateApp("Logon - Winman Live");                
-                SendKeys.SendWait(oPass);
-                SendKeys.SendWait("{ENTER}");
-                //DateTime Tthen = DateTime.Now;
-                //do
-                //{
-                //    Application.DoEvents();
-                //} while (Tthen.AddSeconds(1) > DateTime.Now);
-                //keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)1);
-                //keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, (UIntPtr)1);
+                //ActivateApp("Logon - Winman Live");                
+                InputSimulator.SimulateTextEntry(oPass);
+                System.Threading.Thread.Sleep(100);
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.SHIFT);
             }
             else
             {
                 ActivateApp("winman.exe");
-                ActivateApp("Logon - Winman Live");
-                SendKeys.SendWait(oPass);
-                SendKeys.SendWait("{ENTER}");
-            }    
-            
-           
+                //ActivateApp("Logon - Winman Live");                
+                InputSimulator.SimulateTextEntry(oPass);
+                System.Threading.Thread.Sleep(100);                
+            }
+            InputSimulator.SimulateKeyPress(VirtualKeyCode.RETURN);           
         }
 
         static void ActivateApp(string processName)
@@ -77,8 +78,12 @@ namespace WinMan_Loader
 
             // Activate the first application we find with this name
             if (p.Count() > 0)
+            {                
+                ShowWindow(p[0].MainWindowHandle, 9);
                 SetForegroundWindow(p[0].MainWindowHandle);
+            }
         }
+
 
     }
 }
